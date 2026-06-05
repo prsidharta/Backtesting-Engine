c7686eed82b162bc9871b4f260edf192411d8308
Patrick Sidharta <prdsidharta@gmail.com>
2026-06-04 22:05:55 -0400

    feat: design Bollinger and RSI strategy architecture

--- src/S_BollingerRSI.h
+++ src/S_BollingerRSI.h
@@ -1,1 +1,28 @@
 #pragma once
+#include "TradingStrategy.h"
+#include <queue>
+
+class S_BollingerRSI : public TradingStrategy {
+  private:
+    int bPeriod;
+    double bMultiplier;
+
+    int rsiPeriod;
+    int rsiUpper;
+    int rsiLower;
+
+    double prevDayPrice;
+    std::queue<double> bWindowPrices;
+    void UpdateWindow(double dayPrice);
+
+    std::queue<double> rsiGains;
+    std::queue<double> rsiLosses;
+
+    double bStdDev();
+    double bMovingAverage();
+    double rsiFinal();
+
+  public:
+    S_BollingerRSI(int bPeriod, double bMultiplier, int rsiPeriod, int rsiUpper, int rsiLower);
+    int CreateSignal(double dayPrice, double currentShares) override;
+};
