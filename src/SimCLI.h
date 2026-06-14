2d4dd32d5cb04303b65a52511f803e5a8ba3e5dd
Patrick Sidharta <prdsidharta@gmail.com>
2026-06-13 22:14:48 -0400

    docs: add documentation comments to SimCLI.h

--- src/SimCLI.h
+++ src/SimCLI.h
@@ -1,15 +1,28 @@
+
 #pragma once
 
 #include <string>
 #include <vector>
 
+/**
+ * @class SimCLI
+ * @brief Handles specific execution logic based on user input
+ */
 class SimCLI {
   private:
     std::string selectStock;
     int selectStrat;
 
+    /**
+     * @brief Displays available stock options for the user, and saves user requested stock
+     */
     void DisplayStocksMenu();
+
+    /**
+     * @brief Displays available trading strategies (Moving Average or Bollinger + RSI) and saves user requested strat
+     */
     void DisplayStrategyMenu();
+
     void RunSim();
 
   public:
